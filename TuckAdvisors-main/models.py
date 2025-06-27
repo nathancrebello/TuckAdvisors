a13c97e1c3714eb613ce91d1f0e50458f2c5a52f
nathancrebello <nathan.c.rebello@gmail.com>
2025-06-27 12:50:17 -0400

    models for db

---
+++ TuckAdvisors-main/models.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Integer, Text, Column
+from db import Base
+
+# model representing gptOutput table
+class gptOutput(Base):
+    __tablename__ = "gptOutput"
+    id = Column(Integer, primary_key=True, index=True)
+    content = Column(Text, nullable = False)
