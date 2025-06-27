9fe0b4ca4582c6b315c8e913ab531e2f056471f4
nathancrebello <nathan.c.rebello@gmail.com>
2025-06-27 12:48:19 -0400

    Database for persistent storage

---
+++ TuckAdvisors-main/db.py
@@ -0,0 +1,15 @@
+# imports
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# connection string
+DATABASE_URL = "sqlite:///./gptOutput.db"
+
+# SQLAlchemy engine
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+# session factory    
+LocalSession = sessionmaker(autocommit = False, autoflush=False, bind=engine)
+# base class
+Base = declarative_base()
